7fb72883f8e213d776b5c19229efea69ef1a19dc
RahulGattu912 <rahulgattu912@gmail.com>
2025-11-27 19:13:34 +0530

    initial commit

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+    sg_id = "sg-061fd314500b76b77" # replace with your sg id
+    subnet_id = "subnet-0e0c075ebb865a44b" # replcae with your subnet id
+}
---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "expense-remote-state-dev-project"
+    key    = "expense-tools"
+    region = "us-east-1"
+    dynamodb_table = "expense-remote-state-dev-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
